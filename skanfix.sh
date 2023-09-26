0d5db2043b1fbe65def4c2aed707fb52fab6f0fc
G0LB <144408638+G0LB@users.noreply.github.com>
2023-09-25 22:25:26 -0500

    Update skanfix.sh

--- skanfix.sh
+++ skanfix.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# arp-scan
+# Escanear la red usando arp-scan
 echo "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Local Network Scan ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓"
 sudo arp-scan -l
 
-# IP
+# Solicitar al usuario que ingrese una dirección IP
 echo "▓▓▓▓▓▓▓▓ Enter One Of The IP Addresses to scan (For Example, 192.168.1.1): ▓▓▓▓▓▓▓▓"
 echo ""
 read -p "̿' ̿'\̵͇̿̿\з=(◣_◢)=ε/̵͇̿̿/'̿'̿ ̿ : " ip_address
 
-#  IP ok
+# Verificar que se ingresó una dirección IP válida
 if [[ ! $ip_address =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo "The Entered IP Address Is Not Valid."
     exit 1
 fi
 
-# IP
+# Función para verificar el TTL de la dirección IP ingresada
 function check_ttl {
     local ip="$1"
     echo "▓▓▓▓▓▓▓▓ TTL ▓▓▓▓▓▓▓▓ :(　-_･) ︻デ═一                     *     "
     ttl=$(ping -c 1 $ip | grep -oE "ttl=[0-9]{2,3}" | sed 's/ttl=//')
     echo "   : $ttl :"
-    echo "▓▓▓▓▓▓▓▓ SO ▓▓▓▓▓▓▓▓"
+    echo "===== SO ====="
     
     # Detectar el sistema operativo basado en el valor TTL
     if [ "$ttl" -eq 64 ]; then
@@ -30,6 +30,10 @@ function check_ttl {
         os="Windows NT - less"    
     elif [ "$ttl" -eq 60 ]; then
         os="AIX / HP-UX"
+    elif [ "$ttl" -eq 115 ]; then
+        os="Google"
+    elif [ "$ttl" -eq 64 ]; then
+        os="Router"
     elif [ "$ttl" -eq 128 ]; then
         os="Windows"
     elif [ "$ttl" -eq 255 ]; then
@@ -71,41 +75,39 @@ function ports_line {
     fi
 }
 
-# 
+# Ejecutar la función para verificar el TTL de la dirección IP ingresada
 check_ttl "$ip_address"
 
-# 
+# Ejecutar la función para escanear los puertos de la dirección IP ingresada
 scan_ports "$ip_address"
 
-# 
+# Ejecutar la función para escanear los puertos de la dirección IP ingresada e imprime en linea
 ports_line "$ip_address"
 echo ""
 echo ""
-echo ".     (◣_◢)                            									J'r0ulf"
+echo ".     (◣_◢)"
 echo "░░░░░███████ ]▄▄▄▄▄▄=====================================░ ░ ▒▓▓█ G07B"
 echo "  ▂▄▅████████▅▄▃ ..............."
 echo "███████████████████]>"
 echo "◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤.."
 echo ""
 
-# 
+# Solicitar al usuario que ingrese los puertos a escanear para la consulta adicional
 echo "▓▓▓▓▓▓▓▓ Enter Ports to Scan (For Example, 135,139,445) ▓▓▓▓▓▓▓▓"
-echo ""
 read -p "====> " additional_ports
 
-#
+# Solicitar al usuario el nombre del archivo de salida para la consulta adicional
 echo "Enter The Name of The Output File: "
-echo ""
 read -p "FILE NAME: " output_file_name
 
-#
+# Ejecutar la consulta adicional de nmap con los puertos y el nombre del archivo de salida ingresados
 echo "<(　-_･) ▄︻┻═┳一一                          *     (/❛o❛)/"
 echo "▓▓▓▓▓▓▓▓▓▓ Analyzing Possible Vulnerabilities ▓▓▓▓▓▓▓▓▓▓"
 echo ""
 sudo nmap -sV --script vuln -v --min-rate 6000 -p $additional_ports $ip_address -oA "$output_file_name"
 echo "The File Resulting From The Analysis Has The Name Of $output_file_name"
 
-# 
+# Convertir el archivo XML resultante a HTML
 echo "====================================="
 echo "     Sharing File XML a HTML..."
 echo "====================================="
@@ -124,7 +126,7 @@ echo "░░HTML░░"
 echo "░░░░░░░░"
 echo "░░░░░░░░"
 echo ""
-#
+# Imprimir el nombre del archivo HTML generado
 echo "░░░░░░░░ The Generated HTML File Is: $output_file_name.html "
 echo ""
 echo "........z Z z"
